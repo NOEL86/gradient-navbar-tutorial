a287278342e3d264a8e7490fdafca57d91dddd59
Ashley Jimenez <anoelj86@yahoo.com>
2020-01-06 10:46:56 -0700

    tutorial on css gradient navbar

---
+++ jiggy.js
@@ -0,0 +1,39 @@
+const sections = document.querySelectorAll("section");
+const bubble = document.querySelector(".bubble");
+const gradient = [
+  "linear-gradient(to right top, #bbd2c5, #536976)",
+  "linear-gradient(to right top, #b79891, #94716b)",
+  "linear-gradient(to right top, #acb6e5, #86fde8)"
+];
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+  entries.forEach(entry => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const gradientIndex = entry.target.getAttribute("data-index");
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left
+    };
+
+    if (entry.isIntersecting) {
+      bubble.style.setProperty("left", `${directions.left}px`);
+      bubble.style.setProperty("width", `${directions.width}px`);
+      bubble.style.setProperty("height", `${directions.height}px`);
+      bubble.style.setProperty("top", `${directions.top}px`);
+    }
+  });
+}
+
+sections.forEach(section => {
+  observer.observe(section);
+});
